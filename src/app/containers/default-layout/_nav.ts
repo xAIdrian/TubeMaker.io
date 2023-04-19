737fbe089e20222df6de2fce894c5f19ad70c8a1
Adrian Mohnacs <amohnacs@gmail.com>
2023-04-18 23:40:11 -0500

    open up resources

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -43,131 +43,131 @@ export const navItems: INavData[] = [
     url: '/forms',
     iconComponent: { name: 'cil-notes' }
   },
-  // {
-  //   name: 'Base',
-  //   url: '/base',
-  //   iconComponent: { name: 'cil-puzzle' },
-  //   children: [
-  //     {
-  //       name: 'Accordion',
-  //       url: '/base/accordion'
-  //     },
-  //     {
-  //       name: 'Breadcrumbs',
-  //       url: '/base/breadcrumbs'
-  //     },
-  //     {
-  //       name: 'Cards',
-  //       url: '/base/cards'
-  //     },
-  //     {
-  //       name: 'Carousel',
-  //       url: '/base/carousel'
-  //     },
-  //     {
-  //       name: 'Collapse',
-  //       url: '/base/collapse'
-  //     },
-  //     {
-  //       name: 'List Group',
-  //       url: '/base/list-group'
-  //     },
-  //     {
-  //       name: 'Navs & Tabs',
-  //       url: '/base/navs'
-  //     },
-  //     {
-  //       name: 'Pagination',
-  //       url: '/base/pagination'
-  //     },
-  //     {
-  //       name: 'Placeholder',
-  //       url: '/base/placeholder'
-  //     },
-  //     {
-  //       name: 'Popovers',
-  //       url: '/base/popovers'
-  //     },
-  //     {
-  //       name: 'Progress',
-  //       url: '/base/progress'
-  //     },
-  //     {
-  //       name: 'Spinners',
-  //       url: '/base/spinners'
-  //     },
-  //     {
-  //       name: 'Tables',
-  //       url: '/base/tables'
-  //     },
-  //     {
-  //       name: 'Tabs',
-  //       url: '/base/tabs'
-  //     },
-  //     {
-  //       name: 'Tooltips',
-  //       url: '/base/tooltips'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Buttons',
-  //   url: '/buttons',
-  //   iconComponent: { name: 'cil-cursor' },
-  //   children: [
-  //     {
-  //       name: 'Buttons',
-  //       url: '/buttons/buttons'
-  //     },
-  //     {
-  //       name: 'Button groups',
-  //       url: '/buttons/button-groups'
-  //     },
-  //     {
-  //       name: 'Dropdowns',
-  //       url: '/buttons/dropdowns'
-  //     },
-  //   ]
-  // },
-  // {
-  //   name: 'Forms',
-  //   url: '/forms',
-  //   iconComponent: { name: 'cil-notes' },
-  //   children: [
-  //     {
-  //       name: 'Form Control',
-  //       url: '/forms/form-control'
-  //     },
-  //     {
-  //       name: 'Select',
-  //       url: '/forms/select'
-  //     },
-  //     {
-  //       name: 'Checks & Radios',
-  //       url: '/forms/checks-radios'
-  //     },
-  //     {
-  //       name: 'Range',
-  //       url: '/forms/range'
-  //     },
-  //     {
-  //       name: 'Input Group',
-  //       url: '/forms/input-group'
-  //     },
-  //     {
-  //       name: 'Floating Labels',
-  //       url: '/forms/floating-labels'
-  //     },
-  //     {
-  //       name: 'Layout',
-  //       url: '/forms/layout'
-  //     },
-  //     {
-  //       name: 'Validation',
-  //       url: '/forms/validation'
-  //     }
-  //   ]
-  // },
+  {
+    name: 'Base',
+    url: '/base',
+    iconComponent: { name: 'cil-puzzle' },
+    children: [
+      {
+        name: 'Accordion',
+        url: '/base/accordion'
+      },
+      {
+        name: 'Breadcrumbs',
+        url: '/base/breadcrumbs'
+      },
+      {
+        name: 'Cards',
+        url: '/base/cards'
+      },
+      {
+        name: 'Carousel',
+        url: '/base/carousel'
+      },
+      {
+        name: 'Collapse',
+        url: '/base/collapse'
+      },
+      {
+        name: 'List Group',
+        url: '/base/list-group'
+      },
+      {
+        name: 'Navs & Tabs',
+        url: '/base/navs'
+      },
+      {
+        name: 'Pagination',
+        url: '/base/pagination'
+      },
+      {
+        name: 'Placeholder',
+        url: '/base/placeholder'
+      },
+      {
+        name: 'Popovers',
+        url: '/base/popovers'
+      },
+      {
+        name: 'Progress',
+        url: '/base/progress'
+      },
+      {
+        name: 'Spinners',
+        url: '/base/spinners'
+      },
+      {
+        name: 'Tables',
+        url: '/base/tables'
+      },
+      {
+        name: 'Tabs',
+        url: '/base/tabs'
+      },
+      {
+        name: 'Tooltips',
+        url: '/base/tooltips'
+      }
+    ]
+  },
+  {
+    name: 'Buttons',
+    url: '/buttons',
+    iconComponent: { name: 'cil-cursor' },
+    children: [
+      {
+        name: 'Buttons',
+        url: '/buttons/buttons'
+      },
+      {
+        name: 'Button groups',
+        url: '/buttons/button-groups'
+      },
+      {
+        name: 'Dropdowns',
+        url: '/buttons/dropdowns'
+      },
+    ]
+  },
+  {
+    name: 'Forms',
+    url: '/forms',
+    iconComponent: { name: 'cil-notes' },
+    children: [
+      {
+        name: 'Form Control',
+        url: '/forms/form-control'
+      },
+      {
+        name: 'Select',
+        url: '/forms/select'
+      },
+      {
+        name: 'Checks & Radios',
+        url: '/forms/checks-radios'
+      },
+      {
+        name: 'Range',
+        url: '/forms/range'
+      },
+      {
+        name: 'Input Group',
+        url: '/forms/input-group'
+      },
+      {
+        name: 'Floating Labels',
+        url: '/forms/floating-labels'
+      },
+      {
+        name: 'Layout',
+        url: '/forms/layout'
+      },
+      {
+        name: 'Validation',
+        url: '/forms/validation'
+      }
+    ]
+  },
   {
     name: 'Trending Topics',
     iconComponent: { name: 'cil-star' },
