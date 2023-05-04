8f530ccf871cbad1026686f82440aca7e5393ddc
Adrian Mohnacs <amohnacs@gmail.com>
2023-05-04 11:19:48 +0700

    Successful chained query to get video details

--- backend/routes/youtube.js
+++ backend/routes/youtube.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
-const { map } = require("rxjs/operators");
+const { map, mergeMap, catchError } = require("rxjs/operators");
+const { from, forkJoin } = require("rxjs");
 const { YOUTUBE_DATA_V3_KEY } = require("../../appsecrets");
 
 const BASE_URL = "https://www.googleapis.com/youtube/v3";
 const SEARCH_URL = `${BASE_URL}/search`;
+const VIDEO_URL = `${BASE_URL}/videos`;
 
 router.get("/videos", async (req, res) => {
   const reqNiche = req.query.niche;
@@ -16,66 +18,131 @@ router.get("/videos", async (req, res) => {
   }
 
   try {
-    const params = {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    };
-
-    const queryParams = new URLSearchParams({
-      part: "snippet",
-      maxResults: "25",
-      order: "viewCount",
-      publishedAfter: reqPublishedAtfter,
-      q: reqNiche,
-      regionCode: "US",
-      relevanceLanguage: "en",
-      type: "video",
-      videoDuration: "medium",
-      videoEmbeddable: true,
-      key: YOUTUBE_DATA_V3_KEY,
-    });
-
-    const apiUrl = `${SEARCH_URL}?${queryParams.toString()}`;
-
-    await fetch(apiUrl, params)
-      .then((res) => {
-        console.log("🚀 ~ file: youtube.js:45 ~ .then ~ res:", res)
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error("Error fetching videos");
-        }
-      })
-      .then((data) => {
-        return data.items.map((item) => ({
-          id: item.id.videoId,
-          title: item.snippet.title,
-          description: item.snippet.description,
-          thumbnail: item.snippet.thumbnails.high.url,
-          publishedAt: item.snippet.publishTime,
-          channelTitle: item.channelTitle,
-          // viewCount: item.statistics.viewCount,
-          // likeCount: item.statistics.likeCount,
-          // dislikeCount: item.statistics.dislikeCount,
-          // commentCount: item.statistics.commentCount,
-        }));
-      })
-      .then((data) => {
-        // Map the data object here
+    fetchCompleteVideoData(reqNiche, reqPublishedAtfter).subscribe(
+      (data) => {
+        console.log("🚀 ~ file: youtube.js:23 ~ router.get ~ data:", data)
         res.status(200).json(data);
-      })
-      .catch((err) => {
-        console.error(err);
-        // Handle errors here
+      },
+      (err) => {
         console.error(err);
         res.status(500).json({ error: "Internal server error" });
-      });
+      }
+    );
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+/**
+ * Gets our list of videos from the YouTube API and checks for errors
+ * @param {string} apiUrl 
+ * @param {URLSearchParams} params 
+ * @returns 
+ */
+function fetchVideoList(niche, publishedAfter) {
+  const params = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const queryParams = new URLSearchParams({
+    part: "snippet",
+    maxResults: "25",
+    order: "viewCount",
+    publishedAfter: publishedAfter,
+    q: niche,
+    regionCode: "US",
+    relevanceLanguage: "en",
+    type: "video",
+    videoDuration: "medium",
+    videoEmbeddable: true,
+    key: YOUTUBE_DATA_V3_KEY,
+  });
+
+  const apiUrl = `${SEARCH_URL}?${queryParams.toString()}`;
+  return from(fetch(apiUrl, params)).pipe(
+    mergeMap((res) => {
+      if (res.ok) {
+        return from(res.json());
+      } else {
+        console.log('Error fetching videos');
+      }
+    })
+  );
+}
+
+/**
+ * Fetch data from youtube API and update our list
+ * @param {*} id 
+ * @param {*} params 
+ * @returns Observable<>
+ */
+function fetchVideoDetails(videoId) {
+  const params = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const queryParams = new URLSearchParams({
+    part: "statistics",
+    maxResults: "1",
+    id: videoId,
+    key: YOUTUBE_DATA_V3_KEY,
+  });
+
+  const apiUrl = `${VIDEO_URL}?${queryParams.toString()}`;
+  return from(
+    fetch(apiUrl, params)
+  ).pipe(
+    mergeMap((res) => {
+      console.log("🚀 ~ file: youtube.js:114 ~ mergeMap ~ res:", res)
+      if (res.ok) {
+        return from(res.json());
+      } else {
+        console.log(`Error updating video ${id}`);
+      }
+    })
+  );
+}
+
+/**
+ * Function that updates the video data for all the items in the input list
+ * @param {Array} videoList
+ */ 
+function mapVideoListToDetails(videoList) {
+  console.log("🚀 ~ file: youtube.js:128 ~ updateVideoList ~ videoList:", videoList)
+  const updateObservables = videoList.map((video) =>
+    fetchVideoDetails(video.id)
+  );
+
+  return forkJoin(updateObservables);
+}
+
+/**
+ * Complete wrapper for our fetch functions
+ * @returns 
+ */
+function fetchCompleteVideoData(niche, publishedAfter) {
+  return fetchVideoList(niche, publishedAfter)
+  .pipe(
+    map((data) =>
+      data.items.map((item) => ({
+        id: item.id.videoId,
+        params: {
+          title: item.snippet.title,
+          description: item.snippet.description,
+          thumbnail: item.snippet.thumbnails.high.url,
+          publishedAt: item.snippet.publishTime,
+          channelTitle: item.channelTitle,
+        },
+      }))
+    ),
+    mergeMap((videoList) => mapVideoListToDetails(videoList))
+  )
+}
 
 module.exports = router;
